ca4b1317d48917fdbd0a54f9a3df6e5dced9d4c2
mahmudshumit <rahmanshumit@gmail.com>
2021-02-23 14:01:26 +0600

    Basic Component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // var person = {
+  //   firstName :'Mahmudur',
+  //   lastName :'Rahman'
+  // }
+  //  var highlight ={
+  //   color :'red',
+  //   backgroundColor:'green'
+  // }
   return (
+  
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Done <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <h1 className='' style={highlight}>My Name is - {person.firstName+" "+person.lastName}</h1>
+        <p style={{color:'red'}}>My First React Paragraph</p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <p>React is Fun</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person() {
+  const personStyle = {
+    border:'2px solid yellow',
+    margin:'10px'
+  }
+  return (
+    
+
+    <div style={personStyle}>
+      <h1>Name: Mahmudur Rahman</h1>
+     <h1> Age: 22</h1>
+     <h1>Club:Real Madrid</h1>
+
+    </div>
+
+  )
+  
 }
 
 export default App;
